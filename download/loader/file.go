9091106dbcd734d3dd4e5972ef5cc8b4ab077631
Warren Kumari <warren@kumari.net>
2026-09-10 17:45:05 -0400

    Fix truncation, improve error handling, remove unused math lib.
    Files were being truncated - eg. dev.txt.gz was 175000002bytes, should be 177679968. This was an off-by-one / miscalculation when sending chunks to the worker pool. Simplifed this logic.
    The chunk calulation should probably be moved to common (its currently in both file.go and s3.go)
    
    Also improved error handling in common:
     * closing resp.Body on error
     * explicitly make file sizes int64 - 'int' on a 32 bit machine is 32bit, and e.g .com is large.

--- download/loader/file.go
+++ download/loader/file.go
@@ -2,7 +2,6 @@ package loader
 
 import (
 	"context"
-	"math"
 	"os"
 	"sync"
 
@@ -52,14 +51,9 @@ func (fl FileLoader) DownloadZone(ctx context.Context, accessToken string) error
 		}()
 	}
 	// Send chunks to the worker pool
-	numChunks := int(max(math.Ceil(float64(fs/int(defaultChunkSize))), 1))
-	for i := range numChunks {
-		start := i * int(defaultChunkSize)
-		if i > 0 {
-			start += 1
-		}
-		end := min(start+int(defaultChunkSize), fs)
-		fl.chunks <- &FileChunk{Start: int64(start), End: int64(end), File: f}
+	for start := int64(0); start < fs; start += defaultChunkSize {
+		end := min(start+defaultChunkSize-1, fs-1)
+		fl.chunks <- &FileChunk{Start: start, End: end, File: f}
 	}
 	// Close worker pool
 	close(fl.chunks)
